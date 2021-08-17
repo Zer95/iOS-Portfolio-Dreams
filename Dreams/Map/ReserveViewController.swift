365ef7c59a55f8edacf8f26e6e733397e9394275
Precial <ggrty10@gmail.com>
2021-08-17 16:28:10 +0900

    Insert Content From ReserveView

--- Dreams/Map/ReserveViewController.swift
+++ Dreams/Map/ReserveViewController.swift
@@ -10,6 +10,7 @@ import FSCalendar
 
 class ReserveViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var calendar: FSCalendar!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,5 +32,46 @@ class ReserveViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
 }
+
+
+extension ReserveViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 7
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "timeSelectCell", for: indexPath) as? timeSelectCell else {
+            return UICollectionViewCell()
+    }
+        cell.time.layer.cornerRadius = 10
+        cell.time.layer.borderWidth = 1.0
+        cell.time.layer.borderColor =  #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+        
+        
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+       
+        return CGSize(width: 90, height: 40)
+    }
+    
+}
+
+
+
+
+class timeSelectCell: UICollectionViewCell {
+    @IBOutlet weak var time:UIButton!
+    
+}
+    
+  
+
 
 
