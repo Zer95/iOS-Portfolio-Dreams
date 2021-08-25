3a20b556fd4229adcdeec7993358c0ca9aaae734
Precial <ggrty10@gmail.com>
2021-08-26 00:22:56 +0900

    Insert Price Format From OrderView

--- Dreams/Shop/OrderViewController.swift
+++ Dreams/Shop/OrderViewController.swift
@@ -18,9 +18,20 @@ class OrderViewController: UIViewController {
     
     @IBOutlet weak var selectPriceLabel: UILabel!
     @IBOutlet weak var totalPriceLabel: UILabel!
+    @IBOutlet weak var orderBtn: UIButton!
+    
+    @IBOutlet weak var productCntLabel: UILabel!
     
     
     var productInfo = ShopInfo(keyName: "", name: "", price: 0, stock: 0, delivery: 0)
+    
+    var totalPrice = 0
+    var productPrice = 0
+    var deliveryPrice = 0
+    
+    var productStockCnt = 0
+    var productSelectCnt = 1
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,11 +42,19 @@ class OrderViewController: UIViewController {
     
     func updateUI() {
         nameLabel.text = productInfo.name
-        priceLabel.text = "\(productInfo.price)원"
+        
+        priceLabel.text = "\(priceFormatter(number: productInfo.price))원"
+        self.productPrice = productInfo.price
+        
         stockLabel.text = "잔여 수량: \(productInfo.stock)"
-        deliveryLabel.text = "+ \(productInfo.delivery)원"
+        self.productStockCnt = productInfo.stock
         
+        deliveryLabel.text = "+ \(priceFormatter(number:productInfo.delivery))원"
+        self.deliveryPrice = productInfo.delivery
         
+        let defaultTotal = productInfo.price + productInfo.delivery
+        totalPriceLabel.text = "\(priceFormatter(number:defaultTotal))원"
+        orderBtn.setTitle("\(priceFormatter(number:defaultTotal))원 결제하기", for: .normal)
     }
     
 
@@ -43,7 +62,46 @@ class OrderViewController: UIViewController {
         dismiss(animated: true, completion: nil)
     }
     
-
+    @IBAction func productPlus(_ sender: Any) {
+        
+        if self.productStockCnt > self.productSelectCnt {
+        self.productSelectCnt += 1
+        let selectProductPrice = self.productPrice * self.productSelectCnt
+        self.totalPrice = selectProductPrice + deliveryPrice
+        
+        self.productCntLabel.text = "\(self.productSelectCnt)"
+        self.selectPriceLabel.text = "\(priceFormatter(number:selectProductPrice))원"
+        self.totalPriceLabel.text = "\(priceFormatter(number:self.totalPrice))원"
+        orderBtn.setTitle("\(priceFormatter(number:self.totalPrice))원 결제하기", for: .normal)
+        } else {
+            print("최대 선택 개수 입니다!")
+        }
+    }
+    
+    
+    @IBAction func productMinus(_ sender: Any) {
+        if self.productSelectCnt > 1 {
+        self.productSelectCnt -= 1
+            let selectProductPrice = self.productPrice * self.productSelectCnt
+            self.totalPrice = selectProductPrice + deliveryPrice
+            
+            self.productCntLabel.text = "\(self.productSelectCnt)"
+            self.selectPriceLabel.text = "\(priceFormatter(number:selectProductPrice))원"
+            self.totalPriceLabel.text = "\(priceFormatter(number:self.totalPrice))원"
+            orderBtn.setTitle("\(priceFormatter(number:self.totalPrice))원 결제하기", for: .normal)
+        } else {
+            print("1개이상은 필수 입니다!")
+        }
+    }
+    
+    // #숫자 단위 계산
+    func priceFormatter(number: Int) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        
+        return numberFormatter.string(from: NSNumber(value: number))!
+    }
+    
 }
 
 
