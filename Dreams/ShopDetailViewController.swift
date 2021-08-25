49a95bf8e6778471cde80f3e126e4b64bfd9a883
Precial <ggrty10@gmail.com>
2021-08-25 15:22:20 +0900

    Insert Default Setting From ShopDetailView

--- Dreams/ShopDetailViewController.swift
+++ Dreams/ShopDetailViewController.swift
@@ -9,12 +9,59 @@ import UIKit
 
 class ShopDetailViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-   
+
     }
     
+}
+
+extension ShopDetailViewController: UICollectionViewDataSource  , UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ShopDetailCell", for: indexPath) as? ShopDetailCell else {
+            return UICollectionViewCell()
+        }
+      //  cell.layer.borderWidth = 1
+          return cell
+        }
+        
+          
+   
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 0)
+//    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let itemSpacing: CGFloat = 10
+       
+        
+        let width: CGFloat = (collectionView.bounds.width - itemSpacing)/2
+        let height: CGFloat = width * 10/7
+        return CGSize(width: width, height: height)
+    }
+}
 
 
+extension ShopDetailViewController: UICollectionViewDelegate {
+  
+        // 셀 클릭시 동작하는 부분
+        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+      
+         
+        }
+    }
+
+class ShopDetailCell: UICollectionViewCell {
+    @IBOutlet weak var ImageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var delivery: UILabel!
 }
